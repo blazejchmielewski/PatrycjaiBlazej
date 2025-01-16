065e302eaf38a2742a42421e1c5bf18486267833
BlazejChmielewskiCFSA <blazej.chmielewski@cfsa.pl>
2025-01-16 09:15:08 +0100

    Popdawiam DOM

--- send-email.js
+++ send-email.js
@@ -1,8 +1,8 @@
 function sendEmail(){
     let params= {
-        names: document.getElementById('quest-name'),
-        phone: document.getElementById('quest-phone'),
-        message: document.getElementById('quest-message')
+        names: document.getElementById('quest-name').value,
+        phone: document.getElementById('quest-phone').value,
+        message: document.getElementById('quest-message').value
     }
 
     emailjs.send("service_qy97rek","template_ew813j9", params).then(alert("Email został wysłany!"))
